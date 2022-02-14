0be7f6d07c537f6636cf9b60755b1a7ec2e71468
zhangwd24 <zhangwd24@sany.com.cn>
2022-02-14 15:46:27 +0800

    111

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +8,9 @@ export default defineConfig({
       formats: ["es"],
       fileName: (format) => `index.${format}.js`,
     },
+    // Because we try to use it independently, we don’t exclude lit.
     // rollupOptions: {
-    //   external: /^lit/
-    // }
-  }
-})
+    //   external: /^lit/,
+    // },
+  },
+});
